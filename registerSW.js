ac30239d8578aa802b6aeb90a0d171d04d50fc7e
Your Name <you@example.com>
2025-02-21 10:29:01 -0800

    new

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./intern/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://sharfeedback.github.io/intern/sw.js', { scope: '/' })})}
